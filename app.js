c33af73c36ec4214f92254ac00fd04ec1c7ebe66
vivek chaurasiya <vkch849@gmail.com>
2025-01-08 22:34:40 +0530

    regular commit

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true, useUnifiedTopolo
 
 app.set('view engine','ejs') 
 app.set('views',path.resolve("./views"))
+
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
 app.use(CheckForAuthenticationCookie('token'))
